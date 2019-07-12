1ebfbac631678d3bf8fbf4dbedf1059e43da57f9
STPAG\Bernd.Wissmeier <bernd.wissmeier@stp-online.de>
2019-07-12 16:35:33 +0200

    text to speech

--- app/src/main/java/de/bw/say_hello/MainActivity.java
+++ app/src/main/java/de/bw/say_hello/MainActivity.java
@@ -3,12 +3,21 @@ package de.bw.say_hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tts = new TextToSpeech(this, this);
+    }
+
+    @Override
+    public void onInit(int i){
+        tts.speak("haaaaalo", TextToSpeech.QUEUE_FLUSH, null);
     }
 }
